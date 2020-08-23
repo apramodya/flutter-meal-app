3191b7777e7570dbb0fa224ced1126615979893b
Pramodya <pramodya.emedia@gmail.com>
2020-08-23 13:01:28 +0530

    load meals per selected category from dummy meals

--- meal_app/lib/category_meals_screen.dart
+++ meal_app/lib/category_meals_screen.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
 
+import './dummy_data.dart';
 import 'models/category.dart';
-
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
 
   @override
   Widget build(BuildContext context) {
-    final routeArgs = ModalRoute.of(context).settings.arguments as Map<String,Category>;
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, Category>;
     final categoryTitle = routeArgs['category'].title;
+    final categoryId = routeArgs['category'].id;
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text(
-          'recipes for category',
-        ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Text('${categoryMeals[index].title}');
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
